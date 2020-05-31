0dfcd37c55f32121a8c77fc272fc4fda28198925
Jeff Minnis <jeff@plazsoft.com>
2020-05-31 07:39:22 -0500

    Add project files.

---
+++ FacepunchTestAppVB/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.MemoEdit1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(12, 12)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(87, 55)
+        Me.btnUpload.TabIndex = 0
+        Me.btnUpload.Text = "Upload to Workshop"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'MemoEdit1
+        '
+        Me.MemoEdit1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.MemoEdit1.Location = New System.Drawing.Point(0, 136)
+        Me.MemoEdit1.Multiline = True
+        Me.MemoEdit1.Name = "MemoEdit1"
+        Me.MemoEdit1.Size = New System.Drawing.Size(800, 314)
+        Me.MemoEdit1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MemoEdit1)
+        Me.Controls.Add(Me.btnUpload)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents MemoEdit1 As TextBox
+End Class
---
+++ FacepunchTestAppVB/Form1.vb
@@ -0,0 +1,34 @@
+﻿Imports Steamworks.Ugc
+
+Public Class Form1
+
+    Dim publishReasult As PublishResult
+    Dim theRandom As Random = New Random
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Steamworks.SteamClient.Init(714030, True) '252490 test ID    714030 StudyX    'Yargis 369040
+
+        Catch f As System.Exception
+            MsgBox(f.Message)
+
+            Me.Close()
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Steamworks.SteamClient.Shutdown()
+    End Sub
+
+    Private Async Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        Dim previewLocation = "C:\Users\Jeff\Documents\Visual Studio 2017\Projects\StudyxMain\StudyxMain\TestUpload\icon-1719737_640.png"
+        publishReasult = Await Steamworks.Ugc.Editor.NewCommunityFile.WithTitle(
+            "My New Item" & theRandom.Next).
+            WithDescription("test").
+            WithTag("StudyFile").
+            WithContent("C:\Users\Jeff\Documents\Visual Studio 2017\Projects\StudyxMain\StudyxMain\TestUpload").
+        SubmitAsync()
+        '                WithPreviewFile(previewLocation).      
+        MemoEdit1.Text = publishReasult.Result.ToString & vbCrLf
+    End Sub
+
+End Class
